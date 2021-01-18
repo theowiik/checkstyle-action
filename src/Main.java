0e26da7c77eba1dc535b57e78ea1715090c5b469
Theo <theo.le.wiik@gmail.com>
2021-01-18 18:18:28 +0100

    Almost no errors

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
-
-
-
-
-
-
-
-
+/**
+ * Java Doc
+ */
 public class Main {
 
+  /**
+   * Entry point.
+   *
+   * @param args the args.
+   */
   public static void main(String[] args) {
-System.out.println("Hello world!");
-            System.out.println("This is my ill formatted file.");
+    System.out.println("Hello world!");
+    System.out.println("This is my ill formatted file.");
   }
 
-    boomCrash();
+  /**
+   * This is some java doc.
+   * @param test
+   */
+  private void iDontBoomCrash(int test) {
+  }
 
-      private void wasd(){
-System.out.println("wasd");
-      }
+  /**
+   * Java doooooooooooc.
+   */
+  private void wasd() {
+    System.out.println("wasd");
+  }
 }
