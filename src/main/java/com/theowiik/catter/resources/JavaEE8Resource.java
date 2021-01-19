969e2cf54a78430239c3296a131dc8f42cad9b3d
Theo <theo.le.wiik@gmail.com>
2021-01-19 15:16:24 +0100

    Create web project

---
+++ src/main/java/com/theowiik/catter/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.theowiik.catter.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
